7878004e913d4f4ef302d8d7f4631356bb585814
Pasha Litvak <parsifalty@gmail.com>
2025-02-11 10:18:04 +0200

    ioasjfosd

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Hello, world!");
+
+console.log(2 + 3);
+
+alert("Привет, мир!");
+console.log("Hello, console!");
+
+let name = "Pasha";
+let birthYear = 2005;
+let isStudent = true;
+
+let score = prompt("Введите ваш балл:");
+if (score >= 90) {
+  console.log("Отлично!");
+} else if (score >= 70) {
+  console.log("Хорошо");
+} else {
+  console.log("Можно лучше!");
+}
+
+for (let i = 1; i <= 5; i++) {
+  console.log(`Итерация: ${i}`);
+}
